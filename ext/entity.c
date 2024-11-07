79e1fa768ecc6a775f530d27ea430612e4a6cc30
Anton Chalakov <108125173+AntonCha@users.noreply.github.com>
2024-11-07 19:35:28 +0200

    return nil on attribute_dictionary call from materials or definitionlist entities (#14)
    Co-authored-by: Anton Chalakov <anton.chalakov@chaosgroup.com>

--- ext/entity.c
+++ ext/entity.c
@@ -159,6 +159,10 @@ void Sketchup_Entity_attribute_dictionary_Iterator(SUAttributeDictionaryRef dict
 
 static VALUE Sketchup_Entity_attribute_dictionary(int argc, VALUE* argv, VALUE self)
 {
+	if (!rb_obj_is_kind_of(self, rb_path2class(SKETCHUP_MATERIALS)))
+		return Qnil;
+	if (!rb_obj_is_kind_of(self, rb_path2class(SKETCHUP_DEFINITIONLIST)))
+		return Qnil;
 	SUEntityRef entity = {DATA_PTR(self)};
 	struct AttributeDictionaryIterationContext attribute_dictionary_struct = {argv[0], SU_INVALID};
 	FOREACH(SUEntityGetNumAttributeDictionaries, SUEntityGetAttributeDictionaries, SUAttributeDictionaryRef, entity, Sketchup_Entity_attribute_dictionary_Iterator, &attribute_dictionary_struct);
