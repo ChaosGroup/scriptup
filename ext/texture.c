8bebf794f1dd3bf22c2ec4fccd3fc639c14a7fa3
Anton Chalakov <anton.chalakov@chaosgroup.com>
2025-03-28 10:19:20 +0200

    implement average_color, filename, image_height, image_width

--- ext/texture.c
+++ ext/texture.c
@@ -6,12 +6,51 @@
 static VALUE Sketchup_Texture_average_color(VALUE self)
 {
 	SUTextureRef texture = {DATA_PTR(self)};
-	SUColor* color_val;
-	enum SUResult result = SUTextureGetAverageColor(texture, &color_val);
-	if (result != SU_ERROR_NONE || !color_val)
+	VALUE c = rb_obj_alloc(rb_path2class(SKETCHUP_COLOR));
+	SUColor* color = DATA_PTR(c);
+	enum SUResult result = SUTextureGetAverageColor(texture, color);
+	if (result != SU_ERROR_NONE)
 		return Qnil;
 
-	return Data_Wrap_Struct(rb_path2class(SKETCHUP_COLOR), 0, 0, color_val);
+	return c;
+}
+
+static VALUE Sketchup_Texture_filename(VALUE self)
+{
+	SUTextureRef texture = {DATA_PTR(self)};
+	VALUE output;
+	GETSTRING(SUTextureGetFileName, texture, output);
+	return output;
+}
+
+static VALUE Sketchup_Texture_image_height(VALUE self)
+{
+	SUTextureRef texture = {DATA_PTR(self)};
+	size_t width = 0;
+	size_t height = 0;
+	double s_scale = 0.0;
+	double t_scale = 0.0;
+	SUTextureGetDimensions(texture, &width, &height, &s_scale, &t_scale);
+	return ULL2NUM(height);
+}
+
+static VALUE Sketchup_Texture_image_width(VALUE self)
+{
+	SUTextureRef texture = {DATA_PTR(self)};
+	size_t width = 0;
+	size_t height = 0;
+	double s_scale = 0.0;
+	double t_scale = 0.0;
+	SUTextureGetDimensions(texture, &width, &height, &s_scale, &t_scale);
+	return ULL2NUM(width);
+}
+
+static VALUE Sketchup_Texture_image_rep(VALUE self)
+{
+	SUTextureRef texture = {DATA_PTR(self)};
+	SUImageRepRef image = SU_INVALID;
+	SUTextureGetImageRep(texture, &image);
+	return Data_Wrap_Struct(rb_path2class(SKETCHUP_IMAGEREP), 0, 0, image.ptr);
 }
 
 void Texture_Init(VALUE Sketchup, VALUE Sketchup_Entity)
@@ -19,4 +58,8 @@ void Texture_Init(VALUE Sketchup, VALUE Sketchup_Entity)
 	VALUE Sketchup_Texture = rb_define_class_under(Sketchup, TEXTURE, Sketchup_Entity);
 	rb_undef_alloc_func(Sketchup_Texture);
 	rb_define_method(Sketchup_Texture, "average_color", Sketchup_Texture_average_color, 0);
+	rb_define_method(Sketchup_Texture, "filename", Sketchup_Texture_filename, 0);
+	rb_define_method(Sketchup_Texture, "image_height", Sketchup_Texture_image_height, 0);
+	rb_define_method(Sketchup_Texture, "image_width", Sketchup_Texture_image_width, 0);
+	rb_define_method(Sketchup_Texture, "image_rep", Sketchup_Texture_image_rep, 0);
 }
--- test/test_texture.rb
+++ test/test_texture.rb
@@ -5,9 +5,9 @@ class TestTexture < Minitest::Test
 
   def setup
     Sketchup.open_file("#{ENV['TEST_RESOURCES']}/Untitled.skp")
-    material = Sketchup.active_model.materials['Heather_Shirt']
-    material.texture = "#{__dir__}/texture_map.png"
-    @texture = material.texture
+    @material = Sketchup.active_model.materials['Heather_Shirt']
+    @material.texture = "#{ENV['TEST_RESOURCES']}/texture_map.png"
+    @texture = @material.texture
   end
 
   def test_average_color
@@ -15,27 +15,25 @@ class TestTexture < Minitest::Test
   end
 
   def test_filename
-  end
-
-  def test_height
+    assert_instance_of(String, @texture.filename)
   end
 
   def test_image_height
+    assert_equal(1024, @texture.image_height)
   end
 
   def test_image_rep
+    assert_instance_of(Sketchup::ImageRep, @texture.image_repa)
   end
   
   def test_image_width
+    assert_equal(1024, @texture.image_width)
   end
 
   def test_size
   end
 
   def test_valid?
-  end
-
-  def test_width
   end
 
   def test_write
